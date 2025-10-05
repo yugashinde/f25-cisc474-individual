37de413a8d2f6fe30c60524e45cddc4067e684eb
Austin Cory Bart <acbart@vt.edu>
2025-10-05 15:07:29 -0400

    Initial files for new web-start app

---
+++ apps/web-start/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
+import netlify from '@netlify/vite-plugin-tanstack-start';
+
+const config = defineConfig({
+  plugins: [
+    netlify(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
