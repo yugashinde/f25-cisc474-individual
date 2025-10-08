da46d6f493caade9d07b9bb1da968e49f56283ac
yugandharashinde <shindey@udel.edu>
2025-10-07 22:18:46 -0400

    fixed enrollment module bug

--- apps/api/src/enrollment/enrollment.controller.ts
+++ apps/api/src/enrollment/enrollment.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { EnrollmentService } from './enrollment.service';
 
 @Controller('enrollment')
 export class EnrollmentController {
   constructor(private readonly enrollmentService: EnrollmentService) {}
+
+  @Get()
+  async findAll() {
+    return this.enrollmentService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.enrollmentService.findOne(Number(id));
+  }
 }
+
+
--- apps/api/src/enrollment/enrollment.module.ts
+++ apps/api/src/enrollment/enrollment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EnrollmentService } from './enrollment.service';
 import { EnrollmentController } from './enrollment.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [EnrollmentController],
-  providers: [EnrollmentService],
+  providers: [EnrollmentService, PrismaService],
 })
 export class EnrollmentModule {}
