1f7159ab7b556988449aa3b69cfa6c7288f18eeb
yugandharashinde <shindey@udel.edu>
2025-11-12 02:45:10 -0500

    user courses endpoint fixed

--- apps/api/src/course/course.controller.ts
+++ apps/api/src/course/course.controller.ts
@@ -1,25 +1,30 @@
 import { Controller, Get, Post, Body, Patch,Query,  Param, Delete, UseGuards } from '@nestjs/common';
+
 import { CourseService } from './course.service';
 import { CourseCreateIn, CourseUpdateIn } from '@repo/api';
 import { JwtAuthGuard } from '../auth/jwt.strategy.guard';
 import { AuthGuard } from '@nestjs/passport';
-import type { JwtUser } from 'src/auth/jwt.strategy';
-import { CurrentUser } from 'src/auth/current-user.decorator';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('courses') 
 export class CourseController {
   constructor(private courseService: CourseService) {}
 
-  
   @Post()
-  create(@Body() createCourseDto: CourseCreateIn, @CurrentUser() user : JwtUser) {
-    createCourseDto.ownerId = user.userId;
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createCourseDto: CourseCreateIn) {
     return this.courseService.create(createCourseDto);
   }
 
   
-  
+  @Get()
+  async findAll(@Query('ownerId') ownerId?: string) {
+    if (ownerId) {
+      return await this.courseService.findByOwner(ownerId);
+    }
+    return await this.courseService.findAll();
+  }
+
   @Get(':courseId')
   async findOne(@Param('courseId') courseId: string) {
     return await this.courseService.findOne(courseId);
